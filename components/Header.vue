a6efd2a5b6749fbb84737bbfe6cf4e8e0034047f
Przemysław Janowski <przemyslaw.janowski@pubnub.com>
2020-11-17 15:37:43 +0100

    initial commit

---
+++ components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="mb-16">
+      <nav
+        class="w-full flex items-center justify-between flex-wrap bg-black p-6 rounded-xl"
+      >
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <img src="~/assets/cat-ico.png" class="w-12" />
+          <NuxtLink to="/">
+            <p class="font-semibold text-xl tracking-tight ml-2">Scrum Zoo</p>
+          </NuxtLink>
+        </div>
+        <div class="block lg:hidden">
+          <button
+            class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+          >
+            <svg
+              class="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div class="text-sm lg:flex-grow">
+            <NuxtLink
+              v-for="link in links"
+              :key="link.slug"
+              :to="link.slug"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              {{ link.title }}
+            </NuxtLink>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [{ slug: '/contact', title: 'Contact' }],
+    }
+  },
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="cont">
+    <div>
+      <Header />
+      <Nuxt />
+    </div>
+    <div class="px-6 pt-4 pb-2">
+      <span
+        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+      >
+        © Scrum Zoo Corporation 2020, Katowice, Brynów
+      </span>
+    </div>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  background: #e6f1f7;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.cont {
+  @apply flex flex-col justify-between min-h-screen py-4 mx-auto;
+  max-width: 1200px;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  @apply my-4;
+}
+
+h1 {
+  @apply text-2xl;
+}
+
+h2 {
+  @apply text-xl;
+}
+
+h3 {
+  @apply text-lg;
+}
+</style>
